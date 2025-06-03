414437ac900e50d99251116ca53783af1bf6d141
yotam-curalife <yotam@curalife.com>
2025-06-03 12:44:25 +0200

    .

--- assets/quiz.js
+++ assets/quiz.js
@@ -2982,7 +2982,19 @@ class ProductQuiz {
 				})
 				.join("");
 
-			dropdown.innerHTML = resultsHTML;
+			// Add search hint for initial list
+			const searchHint = !query
+				? `
+				<div class="quiz-payer-search-hint">
+					<svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+						<path d="M7.333 12.667A5.333 5.333 0 1 0 7.333 2a5.333 5.333 0 0 0 0 10.667ZM14 14l-2.9-2.9" stroke="#64748b" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+					</svg>
+					Start typing to search for more plans...
+				</div>
+			`
+				: "";
+
+			dropdown.innerHTML = resultsHTML + searchHint;
 
 			// Attach click listeners to results
 			const resultItems = dropdown.querySelectorAll(".quiz-payer-search-item");
