a97538f35d3e71ae086659d9ca38fc48b7660e9a
yotam-curalife <yotam@curalife.com>
2025-06-04 15:22:26 +0200

    .

--- assets/quiz.js
+++ assets/quiz.js
@@ -1784,8 +1784,8 @@ class ProductQuiz {
 					</div>
 					<div class="quiz-faq-toggle">
                             <svg class="quiz-faq-toggle-icon" width="24" height="24" viewBox="0 0 24 24" fill="none">
-							<path d="M4 12H20" stroke="#454545" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
-							<path d="M12 4V20" stroke="#454545" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M4 12H20" stroke="#4f4f4f" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+							<path d="M12 4V20" stroke="#4f4f4f" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
 						</svg>
 					</div>
 				</div>
