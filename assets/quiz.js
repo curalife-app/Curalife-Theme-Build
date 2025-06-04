8439a88e1defc582f85ea41bec6c09e805f84a01
yotam-curalife <yotam@curalife.com>
2025-06-04 11:58:06 +0200

    .

--- assets/quiz.js
+++ assets/quiz.js
@@ -1723,9 +1723,9 @@ class ProductQuiz {
                        value="${displayValue}"
 					autocomplete="off"
                        aria-describedby="error-${question.id}">
-                <svg class="quiz-payer-search-search-icon" width="16" height="16" viewBox="0 0 16 16" fill="none">
-                    <path d="M7.333 12.667A5.333 5.333 0 1 0 7.333 2a5.333 5.333 0 0 0 0 10.667ZM14 14l-2.9-2.9" stroke="currentColor" stroke-width="1.33" stroke-linecap="round" stroke-linejoin="round"/>
-                </svg>
+                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
+  <path d="M14.1667 14.1667L17.5 17.5M15.8333 9.16667C15.8333 5.48477 12.8486 2.5 9.16667 2.5C5.48477 2.5 2.5 5.48477 2.5 9.16667C2.5 12.8486 5.48477 15.8333 9.16667 15.8333C12.8486 15.8333 15.8333 12.8486 15.8333 9.16667Z" stroke="#121212" stroke-width="1.25" stroke-linecap="round" stroke-linejoin="round"/>
+</svg>
 				<div class="quiz-payer-search-dropdown" id="search-dropdown-${question.id}" style="display: none;">
                     <div class="quiz-payer-search-results"></div>
 				</div>
