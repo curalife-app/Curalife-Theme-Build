534c1bb59aadc4af88cfb9dceec4ba3b8f033268
yotam-curalife <yotam@curalife.com>
2025-06-11 23:14:49 +0200

    .

--- assets/notifications-bundle.js
+++ assets/notifications-bundle.js
@@ -1,11 +1,1 @@
-function __vite_legacy_guard() {
-  import.meta.url;
-  import("_").catch(() => 1);
-  (async function* () {
-  })().next();
-}
-;
-console.log("🔔 Notifications bundle loaded - Notification system initialized");
-export {
-  __vite_legacy_guard
-};
+function i(){import.meta.url,import("_").catch(()=>1),async function*(){}().next()}console.log("🔔 Notifications bundle loaded - Notification system initialized");export{i as __vite_legacy_guard};
